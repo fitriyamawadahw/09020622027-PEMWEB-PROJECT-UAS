cc1cdba282928c73fb2c38099a3c4687cf6075bd
Fitriya Mawadah Warohmah <146052469+fitriyamawadahw@users.noreply.github.com>
2024-06-15 16:32:20 +0700

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import ItemList from './component/ItemList';
+
 
 function App() {
-    const [news, setNews] = useState([]);
-
-    useEffect(() => {
-        fetch('http://localhost:3000/news')
-            .then(response => response.json())
-            .then(data => setNews(data))
-            .catch(error => console.error('Error fetching news:', error));
-    }, []);
-
-    return (
-        <div>
-            <h1>News List</h1>
-            <ul>
-                {news.map(item => (
-                    <li key={item.id_berita}>
-                        <h2>{item.judul_berita}</h2>
-                        <p>{item.ringkasan}</p>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+  return (
+    <div className="App">
+        <h1>News List</h1>
+      <main>
+        <ItemList/>
+      </main>
+    </div>
+  );
 }
 
 export default App;
